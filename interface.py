64ee63b6522d4f5335db8d11c408ae03d2ce681d
Rafael <rbinfantini@gmail.com>
2026-05-29 22:40:13 -0300

    feat: color feedback for answears

--- interface.py
+++ interface.py
@@ -7,3 +7,9 @@ def mostrar_boas_vindas():
     print(Fore.CYAN + Style.BRIGHT + "=" * 50)
     print(Fore.YELLOW + Style.BRIGHT + "       BEM-VINDO AO GITQUIZ       ")
     print(Fore.CYAN + Style.BRIGHT + "=" * 50 + "\n")
+
+def mostrar_acerto():
+    print(Fore.GREEN + Style.BRIGHT + "✔ Resposta Correta!\n")
+
+def mostrar_erro():
+    print(Fore.RED + Style.BRIGHT + "✖ Resposta Incorreta.\n")
