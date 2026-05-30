4ae137ebdf4faf16c3a9f79f61f80ee91f42ff5b
Rafael <rbinfantini@gmail.com>
2026-05-29 22:41:52 -0300

    feat: point summary on game over. Closes #2

--- interface.py
+++ interface.py
@@ -9,7 +9,14 @@ def mostrar_boas_vindas():
     print(Fore.CYAN + Style.BRIGHT + "=" * 50 + "\n")
 
 def mostrar_acerto():
-    print(Fore.GREEN + Style.BRIGHT + "✔ Resposta Correta!\n")
+    print(Fore.GREEN + Style.BRIGHT + "Resposta Correta!\n")
 
 def mostrar_erro():
-    print(Fore.RED + Style.BRIGHT + "✖ Resposta Incorreta.\n")
+    print(Fore.RED + Style.BRIGHT + "Resposta Incorreta.\n")
+
+def mostrar_game_over(resultado):
+    print("\n" + Fore.MAGENTA + Style.BRIGHT + "=" * 50)
+    print(Fore.YELLOW + Style.BRIGHT + "                  FIM DE JOGO                   ")
+    print(Fore.CYAN + f"   Você acertou {resultado['acertos']} de {resultado['total']} perguntas.")
+    print(Fore.GREEN + f"   Pontuação Final: {resultado['pontuacao']}%")
+    print(Fore.MAGENTA + Style.BRIGHT + "=" * 50 + "\n")
